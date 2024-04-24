fbee4e1c1a108c69aae0ec9aa574161209210485
Афсатарова Алина <alina-aly777@yandex.ru>
2024-04-24 20:27:42 +0300

    Add models, urls, serializers for genre, category, slug.

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,2 +1,20 @@
+from rest_framework import viewsets
+
 from .permissions import (AdminOnly, AuthorAdminModeratorOrReadOnly)
+from .serializers import CategorySerializer, GenreSerializer, TitleSerializer
 from reviews.models import (User, Category, Genre, Title, Review, Comment)
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
