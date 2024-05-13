0bf5ecbec1e6fa459a7350916c0ea459ad236f76
Афсатарова Алина <alina-aly777@yandex.ru>
2024-05-13 18:04:42 +0300

    small fixes

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.tokens import default_token_generator
-from django.db.models import Avg, Value
+from django.db.models import Avg
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, mixins, status, viewsets
 from rest_framework.decorators import action
